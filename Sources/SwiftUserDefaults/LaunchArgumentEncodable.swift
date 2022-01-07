4c64df2a637f90dea5b1d221addb0230236e571f
Liam Nichols <liam.nichols.ln@gmail.com>
2022-01-07 12:21:13 +0100

    Update LaunchArgumentEncodable to expose userDefaultOverrides property to seperate usage of Mirror and allow for possible customisation

--- Sources/SwiftUserDefaults/LaunchArgumentEncodable.swift
+++ Sources/SwiftUserDefaults/LaunchArgumentEncodable.swift
@@ -8,11 +8,24 @@ public protocol LaunchArgumentEncodable {
     ///
     /// A default implementation is provided that returns an empty array.
     var additionalLaunchArguments: [String] { get }
+
+    /// An array of types that represent UserDefault key/value overrides to be converted into launch arguments.
+    ///
+    /// A default implementation is provided that uses reflection to collect these values from the receiver.
+    /// You are free to override and provide your own implementation if you would prefer.
+    var userDefaultOverrides: [UserDefaultOverrideRepresentable] { get }
 }
 
 public extension LaunchArgumentEncodable {
     var additionalLaunchArguments: [String] {
         []
+    }
+
+    /// Uses reflection to collect properties that conform to `UserDefaultOverrideRepresentable` from the receiver.
+    var userDefaultOverrides: [UserDefaultOverrideRepresentable] {
+        Mirror(reflecting: self)
+            .children
+            .compactMap { $0.value as? UserDefaultOverrideRepresentable }
     }
 
     /// Collects the complete array of launch arguments from the receiver.
@@ -21,15 +34,15 @@ public extension LaunchArgumentEncodable {
     ///
     /// In addition to overrides, the contents of `additionalLaunchArguments` is appended to the return value.
     func encodeLaunchArguments() throws -> [String] {
-        let mirror = Mirror(reflecting: self)
+        // Map the overrides into a container
         var container = UserDefaults.ValueContainer()
-
-        for child in mirror.children {
-            if let override = child.value as? UserDefaultOverrideRepresentable, let value = try override.getValue() {
-                container.set(value, forKey: override.key)
-            }
+        for userDefaultOverride in userDefaultOverrides {
+            // Add the storable value into the container only if it wasn't nil
+            guard let value = try userDefaultOverride.getValue() else { continue }
+            container.set(value, forKey: userDefaultOverride.key)
         }
 
+        // Return the collected user default overrides along with any additional arguments
         return container.launchArguments + additionalLaunchArguments
     }
 }
